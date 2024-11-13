6ce4c9bb14e86e05aa3ad4fcc638f0dfff7a8661
한재준 <gkswowns25@naver.com>
2024-11-14 01:06:16 +0900

    Initial commit

---
+++ src/main/java/com/example/kkkamza/KkkamzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.kkkamza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkkamzaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkkamzaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kkkamza/KkkamzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kkkamza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkkamzaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
